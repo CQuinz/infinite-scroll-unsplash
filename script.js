b8458de680a0848c932521a74e63af74679cf6ed
Christian <christian.quinlan36@gmail.com>
2020-11-26 16:12:48 +0000

    Starting creating forEach function to loop through the array and display photos

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+const imageContainer = document.getElementById("image-container");
+const loader = document.getElementById("loader");
+
+let photoArray = [];
+
+// Create Elements for links and photos, add to the DOM
+const displayPhotos = ()=>{
+  // Run function foreach object in photoArray
+  photoArray.forEach((photo) =>{
+    
+  });
+}
+
 // Unsplash API
 const apiKey = 'vAVFlouWTXnC8palIWu8TrGUP-2NgDGpMTKAxX8CW3g';
 const count = 10;
@@ -7,8 +20,8 @@ const unsplashUrl = `https://api.unsplash.com/photos/random/?client_id=${apiKey}
 async function getPhotos(){
   try{
     const response = await fetch(unsplashUrl);
-    const data = await response.json();
-    console.log(data);
+    photoArray = await response.json();
+    displayPhotos();
   }catch(error) {
     // Catch error here
   }
