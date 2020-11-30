d9b2a35d6b2e966a34a4aab94029e5ad0bb715f9
Christian <christian.quinlan36@gmail.com>
2020-11-30 19:28:22 +0000

    add an event listener for scrolling with a function to check if near bottom of the page

--- script.js
+++ script.js
@@ -55,5 +55,16 @@ async function getPhotos(){
   }
 }
 
+// Check to see if scrolling near bottom of the page. Load more photos
+window.addEventListener('scroll', ()=>{
+  if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000){
+    console.log('window.innerHeight: '+ window.innerHeight);
+    console.log('window.scrollY: '+ window.scrollY);
+    console.log('window.innerHeight + window.scrollY: '+ window.innerHeight + window.scrollY);
+    console.log('document.body.offsetHeight: '+ document.body.offsetHeight);
+
+  }
+});
+
 // On load
-getPhotos();
+// getPhotos();
